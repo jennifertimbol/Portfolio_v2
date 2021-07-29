8e4fff291074cde4dfaeb8e0c5cd673c771ab05e
jennifertimbol <jennifertmbl@gmail.com>
2021-07-29 14:22:58 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
